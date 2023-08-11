dd5d65fad939a1706ae2620936e5904ffaedee27
DelayStepback <steblivec1000@gmail.com>
2023-08-11 20:32:34 +0300

    feat: added bloc for cocktails

---
+++ lib/features/presentation/bloc/cocktails_state.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+import '../../data/cocktail/model/cocktail.dart';
+
+class CocktailsState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class CocktailsLoadingState extends CocktailsState {
+  @override
+  List<Object?> get props => [];
+}
+
+
+class CocktailsLoadedState extends CocktailsState {
+  final List<Cocktail>? allCocktails;
+
+  CocktailsLoadedState({required this.allCocktails});
+
+  @override
+  List<Object?> get props => [allCocktails];
+}
+
+class CocktailsFavouriteLoadedState extends CocktailsState {
+  final List<Cocktail>? allFavouriteCocktails;
+
+  CocktailsFavouriteLoadedState({required this.allFavouriteCocktails});
+
+  @override
+  List<Object?> get props => [allFavouriteCocktails];
+}
+
+class CocktailsErrorState extends CocktailsState {
+  final String error;
+
+  CocktailsErrorState(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
