b0d71b9577116e93a315f9749888d32d0ec6874a
Ivan <steblivec1000@gmail.com>
2024-05-09 10:25:44 +0300

    fix: update colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
-Color mainColor = Color(0xFFC1C1C1);
+Color mainColor = const Color(0xFFC1C1C1);
 class MyColor {
-  static Color mainColor = Color(0xFFFFFFFF);
+  static Color mainColor = const Color(0xFFFFFFFF);
   static const Color accentColor = Color(0xFF3F5C85);
   static const Color accentColor2 = Color(0xFF2C538C);
   static const Color iconsColor = Color(0xFF46FFB1);
